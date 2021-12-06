9d7d55fa761a615fe20251c3fe4fb4a1a5f9c040
Daniel Urbanski <urbanski@gmail.com>
2021-12-06 02:49:24 -0800

    Add mod

---
+++ BetterFriendship/BetterFriendship/GameExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using StardewValley;
+using StardewValley.Characters;
+
+namespace BetterFriendship
+{
+    internal static class GameExtensions
+    {
+        public static bool IsTownsfolk(this NPC npc)
+        {
+            return npc is not Pet
+                   && npc is not Horse
+                   && npc is not Junimo
+                   && npc is not JunimoHarvester
+                   && npc is not TrashBear;
+        }
+
+        public static IEnumerable<(Object, int)> TakeTopPrioritized(this IEnumerable<(Object item, int taste)> items,
+            ModConfig config)
+        {
+            if (config.OnlyHighestQuality)
+            {
+                items = items.GroupBy(x => x.item.name)
+                    .Select(x => x.OrderByDescending(y => y.item.Quality).First());
+            }
+
+            return items
+                .OrderBy(x => x.taste)
+                .ThenByDescending(x => x.item.Quality)
+                .ThenBy(x => x.item.salePrice())
+                .Take(config.GiftCycleCount);
+        }
+    }
+}
---
+++ BetterFriendship/BetterFriendship/ModConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace BetterFriendship
+{
+    internal class ModConfig
+    {
+        public bool DisplayTalkPrompts { get; set; } = true;
+        public string GiftPreference { get; set; } = "like";
+        public int GiftCycleCount { get; set; } = 3;
+        public int GiftCycleDelay { get; set; } = 2000;
+        public bool IgnoreMaxedFriendships { get; set; } = true;
+        public bool OnlyHighestQuality { get; set; } = false;
+        public bool DisplayBubbles { get; set; } = true;
+    }
+}
