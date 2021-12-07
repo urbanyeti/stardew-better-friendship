0e0cd148e2910dd23301d7452229d2c636a26c85
Daniel Urbanski <urbanski@gmail.com>
2021-12-06 17:22:58 -0800

    Update default settings, update manifest

--- BetterFriendship/BetterFriendship/ModConfig.cs
+++ BetterFriendship/BetterFriendship/ModConfig.cs
@@ -4,7 +4,7 @@
     {
         public bool DisplayTalkPrompts { get; set; } = true;
         public string GiftPreference { get; set; } = "like";
-        public int GiftCycleCount { get; set; } = 3;
+        public int GiftCycleCount { get; set; } = 5;
         public int GiftCycleDelay { get; set; } = 2000;
         public bool IgnoreMaxedFriendships { get; set; } = true;
         public bool OnlyHighestQuality { get; set; } = false;
