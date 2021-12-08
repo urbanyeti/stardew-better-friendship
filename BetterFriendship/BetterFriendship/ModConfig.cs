f4d94fa1e8254c1d2f6d019c7096bb1b3968cbc4
Daniel Urbanski <urbanski@gmail.com>
2021-12-08 13:09:18 -0800

    Change default to "false" for IgnoreMaxedFriendship

--- BetterFriendship/BetterFriendship/ModConfig.cs
+++ BetterFriendship/BetterFriendship/ModConfig.cs
@@ -6,7 +6,7 @@
         public string GiftPreference { get; set; } = "like";
         public int GiftCycleCount { get; set; } = 5;
         public int GiftCycleDelay { get; set; } = 2000;
-        public bool IgnoreMaxedFriendships { get; set; } = true;
+        public bool IgnoreMaxedFriendships { get; set; } = false;
         public bool OnlyHighestQuality { get; set; } = false;
         public bool DisplayBubbles { get; set; } = true;
     }
